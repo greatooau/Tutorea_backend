52e58d6262ba95f7ac1c99adc0938c4cbf434d04
gretooau <pajaroazulx@gmail.com>
2022-04-14 00:59:26 -0500

    Two more methods

--- src/controllers/tutorController.js
+++ src/controllers/tutorController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
 /**
- * @description getTutors
+ * @description This method is used to get all the tutors from Database.
  * @route GET api/tutors
  * @access Private
  */
@@ -9,10 +9,36 @@ const asyncHandler = require('express-async-handler');
 const getTutors = (req, res) => {
     res.json({
         message:'prueba GET'
-    })
+    });
     res.status(200);
 };
 
+/**
+ * @description This method is used to get just one Tutor by ID.
+ * @route GET api/tutors/:id
+ * @access Private
+ */
+
+ const getTutor = (req, res) => {
+    res.json({
+        message:'prueba GET, un tutor'
+    });
+    res.status(200);
+};
+/**
+ * @description This method is used to register a new tutor.
+ * @route POST api/tutors
+ * @access Private
+ */
+const registerTutor = (req, res) => {
+    res.json({
+        message:'prueba POST'
+    });
+    res.status(200);
+}
+
 module.exports = {
-    getTutors
+    getTutors,
+    getTutor,
+    registerTutor
 }
