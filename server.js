79d11758e8b2750ee7c6ad3e735c05bebc54a2f9
gretooau <pajaroazulx@gmail.com>
2022-04-17 20:43:44 -0500

    server.js modified to use middlewares

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./src/config/db');
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/tutors', require('./src/routes/tutorRoutes'));
-
 app.use('/api/users', require('./src/routes/userRoutes'));
 
+app.use(errorHandler);
 app.listen(port, () => console.log(`Listening on port: ${port}`))
