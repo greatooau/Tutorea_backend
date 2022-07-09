e1f1126c470ebc573a82be39e64f803001a06687
gretooau <pajaroazulx@gmail.com>
2022-07-09 01:25:11 -0500

    falta calar enpoints

--- src/controllers/tutorController.js
+++ src/controllers/tutorController.js
@@ -1,36 +1,35 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const Tutor = require('../models/tutorModel');
-const Insights = require('../models/insightsModel');
-const Contacts = require('../models/contactsModel');
-const Studies = require('../models/studiesModel');
-const Transactions = require('../models/transactionModel')
+const Tutor = require("../models/tutorModel");
+const Insights = require("../models/insightsModel");
+const Contacts = require("../models/contactsModel");
+const Studies = require("../models/studiesModel");
+const Transactions = require("../models/transactionModel");
 const jwt = require("jsonwebtoken");
-
 
 /**
  * @description This method is used to login as a tutor.
  * @route GET api/tutors/login
  * @access Public
  */
-const loginTutor = asyncHandler ( 
-    async (req, res) => {
-        //Recibe usuario y contraseña del tutor
-        const { username, password } = req.body
-        //Busca al tutor en la DB
-        const tutor = await Tutor.findOne({ username: username });
-        //Si el tutor existe, y su contraseña es la correcta, entonces...
-        if (tutor && (await bcrypt.compare(password, tutor.password))) {
-            res.status(200).json({token: generateToken(tutor._id, 'tutor'), role: 'tutor'});
-        }
-        else {
-            res.status(400);
-            throw new Error("Invalid credentials");
-        }
-        
-        res.end()
+const loginTutor = asyncHandler(async (req, res) => {
+    //Recibe usuario y contraseña del tutor
+    const { username, password } = req.body;
+    //Busca al tutor en la DB
+    const tutor = await Tutor.findOne({ username: username });
+    //Si el tutor existe, y su contraseña es la correcta, entonces...
+    if (tutor && (await bcrypt.compare(password, tutor.password))) {
+        res.status(200).json({
+            token: generateToken(tutor._id, "tutor"),
+            role: "tutor",
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid credentials");
     }
-)
+
+    res.end();
+});
 
 //METODO PARA REALIZAR EL PRE REGISTRO DEL TUTOR
 /**
@@ -38,32 +37,31 @@ const loginTutor = asyncHandler (
  * @route GET api/tutors/pre-register
  * @access Public
  */
- const preregister = asyncHandler(async (req, res) => {
+const preregister = asyncHandler(async (req, res) => {
     const {
-      username,//username
-      password,//password
-      name,//name
-      fee, //AÑADIR EN FRONTEND
-      lastname,//lastname
-      category,//category
-      specialization,//specialization
-      email,//email
+        username, //username
+        password, //password
+        name, //name
+        fee, //AÑADIR EN FRONTEND
+        lastname, //lastname
+        category, //category
+        specialization, //specialization
+        email, //email
     } = req.body;
 
     if (
-        !username ||//username
-        !password ||//password
-        !name ||//name
-        !lastname ||//lastname
+        !username || //username
+        !password || //password
+        !name || //name
+        !lastname || //lastname
         !fee ||
-        !category ||//category
-        !specialization ||//specialization
-        !email//email
+        !category || //category
+        !specialization || //specialization
+        !email //email
     ) {
         res.status(400);
         throw new Error("Please add all fields");
     }
-    
 
     const tutorExists = await Tutor.findOne({ email, username });
 
@@ -77,147 +75,190 @@ const loginTutor = asyncHandler (
 
     //CHECAR QUE CONCUERDE CON EL MODELO
     const tutor = await Tutor.create({
-        username,//username
-        password: hashedPassword,//password
-        name,//name
-        lastname,//lastname
-        category,//category
-        specialization,//specialization
-        email,//email
-        phone:"", // NO se si incluirlo, no creo
+        username, //username
+        password: hashedPassword, //password
+        name, //name
+        lastname, //lastname
+        category, //category
+        specialization, //specialization
+        email, //email
+        phone: "", // NO se si incluirlo, no creo
         fee,
-        sex:"",
-        bank_account:"",
-        born_date:"",
-        studies:[],
-        insights:[],
-        contacts:[],
-        status:"PENDIENTE",//ACTIVO, RECHAZADO
-        profile_picture:"https://imgs.search.brave.com/BBWmpwlbk30sxtRiwzETZbbQ9qNDoeUAIpLNqBhzPmo/rs:fit:1200:1200:1/g:ce/aHR0cDovL3N0YXRp/Yy5ndWltLmNvLnVr/L3N5cy1pbWFnZXMv/R3VhcmRpYW4vUGl4/L3BpY3R1cmVzLzIw/MTUvNi85LzE0MzM4/NjA4ODg1MDMvQWxi/ZXJ0LUVpbnN0ZWlu/Li0tMDA5LmpwZw"
-    
+        sex: "",
+        bank_account: "",
+        born_date: "",
+        studies: [],
+        insights: [],
+        contacts: [],
+        status: "PENDIENTE", //ACTIVO, RECHAZADO
+        profile_picture:
+            "https://imgs.search.brave.com/BBWmpwlbk30sxtRiwzETZbbQ9qNDoeUAIpLNqBhzPmo/rs:fit:1200:1200:1/g:ce/aHR0cDovL3N0YXRp/Yy5ndWltLmNvLnVr/L3N5cy1pbWFnZXMv/R3VhcmRpYW4vUGl4/L3BpY3R1cmVzLzIw/MTUvNi85LzE0MzM4/NjA4ODg1MDMvQWxi/ZXJ0LUVpbnN0ZWlu/Li0tMDA5LmpwZw",
     });
 
     if (tutor) {
-      res.status(201).json({
-        _id: tutor.id,
-        name: tutor.name,
-        email: tutor.email,
-        token: generateToken(tutor._id, 'tutor'),
-      });
+        res.status(201).json({
+            _id: tutor.id,
+            name: tutor.name,
+            email: tutor.email,
+            token: generateToken(tutor._id, "tutor"),
+        });
     } else {
-      res.status(400);
-      throw new Error("Invalid user data");
+        res.status(400);
+        throw new Error("Invalid user data");
     }
     res.end();
 });
 
+const addStudies = asyncHandler(async (req, res) => {
+    const { study, school, img } = req.body;
 
-const changePassword = asyncHandler( 
-    async(req, res) => {
-        const oldPassword = req.body.oldPassword
-        const newPassword = req.body.newPassword
-        const user = await Tutor.findById(req.tutor._id)
-        const flag = await bcrypt.compare(oldPassword, user.password)
+    if (!(study && school)) {
+        res.status(400);
+        res.end();
+        throw new Error(
+            "You need to provide both study and school to create a new study."
+        );
+    }
 
-        if(!user && !flag) {
-            res.status(400)
-            res.end()//por si acaso pa
-            throw new Error('Password incorrect')
-        }
-        
-        const salt = await bcrypt.genSalt(10);
-        const hashedNewPassword = await bcrypt.hash(newPassword, salt);
-        console.log(hashedNewPassword)
-        await Tutor.updateOne(
+    await Tutor.updateOne(
+        { _id: req.tutor._id },
+        { $push: { studies: req.body } }
+    );
+    res.status(200).end();
+});
+
+const addContacts = asyncHandler(async (req, res) => {
+    const { type, contact } = req.body;
+
+    if (!(type && contact)) {
+        res.status(400);
+        res.end();
+        throw new Error(
+            "You need to provide both type and contact to create a new contact."
+        );
+    }
+
+    await Tutor.updateOne(
+        { _id: req.tutor._id },
+        { $push: { contacts: req.body } }
+    );
+    res.status(200).end();
+});
+
+const addInsights = asyncHandler(async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        res.status(400);
+        res.end();
+        throw new Error(
+            "You need to provide a name to create a new insight."
+        );
+    }
+
+    await Tutor.updateOne(
+        { _id: req.tutor._id },
+        { $push: { insights: req.body } }
+    );
+    res.status(200).end();
+});
+
+const changePassword = asyncHandler(async (req, res) => {
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword;
+    const user = await Tutor.findById(req.tutor._id);
+    const flag = await bcrypt.compare(oldPassword, user.password);
+
+    if (!user && !flag) {
+        res.status(400);
+        res.end(); //por si acaso pa
+        throw new Error("Password incorrect");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+    console.log(hashedNewPassword);
+    await Tutor.updateOne(
         { _id: req.tutor.id },
         { $set: { password: hashedNewPassword } }
-        )
-        
-        res.status(200)
-        res.json({message: 'Exito al cambiar la contraseña'})
-        res.end()
-    }
-)
+    );
+
+    res.status(200);
+    res.json({ message: "Exito al cambiar la contraseña" });
+    res.end();
+});
 //METODO PARA OBTENER LOS ESTUDIANTES DEL TUTOR
 /**
  * @description This method is used to get all the tutor's students.
  * @route GET api/tutors/students
  * @access Private
  */
-const getMyStudents = asyncHandler (
-    async (req, res) => {
-        id = "6265b3495bce7cc5b6b7d341" // para probar
-        //TODO: Hacer que se tenga la propiedad "active" para el schema de transactions
-        const transactions = await Transactions.find({
-            $and: [{ tutor: id }, { activo: 1 }],
-        }).populate("user");        
-        //Variable auxiliar
-        const students = []
+const getMyStudents = asyncHandler(async (req, res) => {
+    id = "6265b3495bce7cc5b6b7d341"; // para probar
+    //TODO: Hacer que se tenga la propiedad "active" para el schema de transactions
+    const transactions = await Transactions.find({
+        $and: [{ tutor: id }, { activo: 1 }],
+    }).populate("user");
+    //Variable auxiliar
+    const students = [];
 
-        transactions.forEach(transaction => {
-            students.push(
-            { 
-                    id: transaction._id, //id
-                    student: transaction.user, //usuario completo
-                    sesions: transaction.sesions //numero de sesiones
-                }
-            )
+    transactions.forEach((transaction) => {
+        students.push({
+            id: transaction._id, //id
+            student: transaction.user, //usuario completo
+            sesions: transaction.sesions, //numero de sesiones
         });
-        
+    });
 
-        //Enviar 
-        res.json(students);
-        res.status(200);
-        res.end();
-    }
-);
+    //Enviar
+    res.json(students);
+    res.status(200);
+    res.end();
+});
 //FALTA DESARROLLAR PRE-REGISTRO CON CARGA DE ARCHIVOS Y ENVIO AL CORREO
 
 const generateToken = (id, role) => {
     return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-const getMe = asyncHandler(
-    async(req, res) => {
-        const {
-            _id,
-            name,
-            email,
-            profile_picture,
-            lastname,
-            username,
-            specialization,
-            fee,
-            category,
-            sex,
-            born_date,
-            phone,
-            bank_account,
-            status
-        } = await Tutor.findById(req.tutor._id);
-        
-        const response = {
-            id:_id,
-            name,
-            email,
-            profile_picture,
-            lastname,
-            username,
-            specialization,
-            fee,
-            category,
-            sex,
-            born_date,
-            phone,
-            bank_account,
-            status
-        }
-        res.status(200).json(response)
+const getMe = asyncHandler(async (req, res) => {
+    const {
+        _id,
+        name,
+        email,
+        profile_picture,
+        lastname,
+        username,
+        specialization,
+        fee,
+        category,
+        sex,
+        born_date,
+        phone,
+        bank_account,
+        status,
+    } = await Tutor.findById(req.tutor._id);
 
-        res.end()
-    }
-)
+    const response = {
+        id: _id,
+        name,
+        email,
+        profile_picture,
+        lastname,
+        username,
+        specialization,
+        fee,
+        category,
+        sex,
+        born_date,
+        phone,
+        bank_account,
+        status,
+    };
+    res.status(200).json(response);
 
+    res.end();
+});
 
 /**
  * @description This method is used to get all the tutors from Database.
@@ -225,14 +266,16 @@ const getMe = asyncHandler(
  * @access Private
  */
 
-const getTutors = asyncHandler( async(req, res) => {
-    const tutors = await Tutor.find()
-/*     const insights = await Insights.find();
+const getTutors = asyncHandler(async (req, res) => {
+    const tutors = await Tutor.find();
+    /*     const insights = await Insights.find();
     const studies = await Studies.find();
     const contacts = await Contacts.find(); */
-    res.json(tutors /* insights:insights, studies:studies, contacts:contacts */);
+    res.json(
+        tutors /* insights:insights, studies:studies, contacts:contacts */
+    );
     res.status(200);
-    res.end()
+    res.end();
 });
 
 /**
@@ -241,16 +284,15 @@ const getTutors = asyncHandler( async(req, res) => {
  * @access Private
  */
 
-const getTutorsByCategory = asyncHandler( async(req, res) => {
-    const tutors = await Tutor.find({category:req.params.category})
-/*     const insights = await Insights.find();
+const getTutorsByCategory = asyncHandler(async (req, res) => {
+    const tutors = await Tutor.find({ category: req.params.category });
+    /*     const insights = await Insights.find();
     const studies = await Studies.find();
     const contacts = await Contacts.find(); */
     res.json(tutors);
     res.status(200);
-    res.end()
+    res.end();
 });
-
 
 /**
  * @description This method is used to get just one Tutor by ID.
@@ -258,21 +300,20 @@ const getTutorsByCategory = asyncHandler( async(req, res) => {
  * @access Private
  */
 
-const getTutor = asyncHandler( async(req, res) => {
+const getTutor = asyncHandler(async (req, res) => {
     const tutor = await Tutor.findById(req.params.id);
-    if(!tutor) {
+    if (!tutor) {
         res.status(400);
         throw new Error("Tutor doesn't exist");
-    } else {/* 
+    } else {
+        /* 
         const insights = await Insights.find({tutor:req.params.id});
         const studies = await Studies.find({tutor:req.params.id});
         const contacts = await Contacts.find({tutor:req.params.id}); */
         res.status(200).json(tutor);
     }
-    res.end()
+    res.end();
 });
-
-
 
 module.exports = {
     getTutors,
@@ -282,5 +323,8 @@ module.exports = {
     loginTutor,
     getMe,
     changePassword,
-    preregister
-}
+    preregister,
+    addInsights,
+    addContacts,
+    addStudies
+};
--- src/routes/tutorRoutes.js
+++ src/routes/tutorRoutes.js
@@ -1,35 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getTutors, getTutor, getTutorsByCategory, loginTutor, getMyStudents, getMe, changePassword, preregister } = require('../controllers/tutorController');
-const { protect } = require('../middleware/authMiddleware')
+const {
+    getTutors,
+    getTutor,
+    getTutorsByCategory,
+    loginTutor,
+    getMyStudents,
+    getMe,
+    changePassword,
+    preregister,
+    addInsights,
+    addStudies,
+    addContacts,
+} = require("../controllers/tutorController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/')
-    .get(protect, getTutors);
+router.route("/").get(protect, getTutors);
 
-router.route('/me')
-    .get(protect, getMe);
+router.route("/me").get(protect, getMe);
 
-router.route('/students')
-.get(protect, getMyStudents);
+router.route("/students").get(protect, getMyStudents);
 
 //Login del tutor
-router.route('/login')
-    .post(loginTutor);
+router.route("/login").post(loginTutor);
+//TODO: Calar este endpoint
+router.route("/password").post(protect, changePassword);
+//TODO: Calar este endpoint
+router.route("/pre-register").post(preregister);
+//TODO: Calar este endpoint
+router.route("/insights").post(protect, addInsights);
+//TODO: Calar este endpoint
+router.route("/contacts").post(protect, addContacts);
+//TODO: Calar este endpoint
+router.route("/studies").post(protect, addStudies);
 
-router.route('/password')
-    .post(protect, changePassword);
+router.route("/:id").get(protect, getTutor);
 
-
-router.route('/pre-register')
-    .post(preregister);    
-    
-router.route('/:id')
-    .get(protect, getTutor);
-
-router.route('/category/:category')
-    .get(protect, getTutorsByCategory);
-
-
-
+router.route("/category/:category").get(protect, getTutorsByCategory);
 
 module.exports = router;
